bfd4a0d0d3a0ed61e3b4a1c6a36770cb09bcb6e2
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2021-01-20 10:44:59 +0200

    Fix settings for sentry

--- django_server/application_evaluator_config/settings.py
+++ django_server/application_evaluator_config/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
