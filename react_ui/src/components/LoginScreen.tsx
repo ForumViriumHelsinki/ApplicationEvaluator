b1f6df402ce230c95b0f4c25565d86f6c481c5a9
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2021-01-29 13:30:06 +0200

    Remove unneeded registration screen

--- react_ui/src/components/LoginScreen.tsx
+++ react_ui/src/components/LoginScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {loginUrl, registerUrl, passwordResetUrl} from "urls";
 import LoginForm from "util_components/account/LoginForm";
-import RegisterForm from "util_components/account/RegisterForm";
 import Terms from "components/Terms";
 
 type func = () => any;
@@ -9,49 +8,25 @@ type func = () => any;
 type LoginScreenProps = { onLogin: func };
 
 type LoginScreenState = {
-  mode: 'login' | 'register',
   showTerms: boolean
 };
 
 export default class LoginScreen extends React.Component<LoginScreenProps, LoginScreenState> {
   state: LoginScreenState = {
-    mode: 'login',
     showTerms: false
   };
 
   render() {
     const {onLogin} = this.props;
-    const {mode} = this.state;
 
     return (
       <div className="container p-4 mt-5 rounded trans-bg">
         <div className="text-center">
           <img className="w-50" src="images/FORUM_VIRIUM_logo_orange.png" alt="logo"/>
           <h3>FVH Application Evaluator</h3>
-          <p className="lead">
-            {mode == 'login' ?
-              <>
-                <span className="text-primary">Sign in</span> or{' '}
-                <button className="btn btn-outline-primary"
-                        onClick={() => this.setState({mode: 'register'})}>
-                  Register
-                </button>
-              </>
-            :
-              <>
-                <button className="btn btn-outline-primary"
-                        onClick={() => this.setState({mode: 'login'})}>
-                  Sign in
-                </button> or{' '}
-                <span className="text-primary">Register</span>
-              </>
-            }
-          </p>
+          <p className="lead text-primary">Sign in</p>
         </div>
-        {mode == 'login' ?
           <LoginForm loginUrl={loginUrl} onLogin={onLogin} passwordResetUrl={passwordResetUrl}/>
-        : <RegisterForm url={registerUrl} loginUrl={loginUrl} onLogin={onLogin}/>
-        }
         <Terms/>
       </div>
     );
