ce778df65e177b72f99224f95b8b67d177ff9a2f
Aapo (f021b) Rista <aapris@gmail.com>
2023-08-23 13:24:00 +0300

    Start to use pyproject.toml Start to use requirements.txt instead of Pipfile Update Django and other modules Replace django-rest-auth with dj-rest-auth

--- django_server/application_evaluator_config/urls.py
+++ django_server/application_evaluator_config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.views.generic import TemplateView
-from rest_auth.views import LogoutView
+from dj_rest_auth.views import LogoutView
 from rest_framework.schemas import get_schema_view
 
 from application_evaluator import rest
@@ -26,11 +26,13 @@ from application_evaluator import rest
 schema_view = get_schema_view(
     title="FVH Application Evaluator API",
     description="API for interacting with the FVH Application Evaluator application",
-    version="1.0.0", public=True)
+    version="1.0.0",
+    public=True,
+)
 
 
 def error_view(request):
-    raise Exception('This is a test error to verify error reporting.')
+    raise Exception("This is a test error to verify error reporting.")
 
 
 # Allow logging out when faced with the mysterious CSRF cookie corruption problem:
@@ -39,16 +41,17 @@ class LogoutView(LogoutView):
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rest-auth/logout/', LogoutView.as_view()),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest/error_test/', error_view, name='error-view'),
-    path('rest/', include(rest.router.urls)),
-    path('openapi/', schema_view, name='openapi-schema'),
-    path('swagger-ui/', TemplateView.as_view(
-        template_name='swagger-ui.html',
-        extra_context={'schema_url': 'openapi-schema'}
-    ), name='swagger-ui')
+    path("admin/", admin.site.urls),
+    path("rest-auth/logout/", LogoutView.as_view()),
+    path("rest-auth/", include("dj_rest_auth.urls")),
+    path("rest/error_test/", error_view, name="error-view"),
+    path("rest/", include(rest.router.urls)),
+    path("openapi/", schema_view, name="openapi-schema"),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(template_name="swagger-ui.html", extra_context={"schema_url": "openapi-schema"}),
+        name="swagger-ui",
+    ),
 ]
 
 if settings.DEBUG:
